93ac1c2f1d218abf957a705f7581cad100314e5a
Binyam <binyamfitamo7@gmail.com>
2022-03-15 11:01:37 -0700

    putchar exercise

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * main - function prints out _putchar on new line
  * Return: 0 is success
  */
 int main(void)
 {
-	printf("/n_putchar");
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
+	putchar("\n")
 	return (0);
 }
